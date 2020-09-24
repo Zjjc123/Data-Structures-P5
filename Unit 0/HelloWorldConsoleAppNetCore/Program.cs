1806d0675e07a9a86c98dd1b390386feddfaf34d
Jason Zhang <zjjc123@hotmail.com>
2020-09-24 12:16:36 -0700

    Another Hello World Project

---
+++ Unit 0/HelloWorldConsoleAppNetCore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorldConsoleAppNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press enter to continue...");
+            Console.Read();
+        }
+    }
+}
