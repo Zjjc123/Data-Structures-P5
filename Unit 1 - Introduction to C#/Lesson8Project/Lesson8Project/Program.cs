c614e1d0320c76576d3d7adffc72cecc85abbe87
Jason Zhang <zjjc123@hotmail.com>
2020-11-02 21:54:29 -0800

    U2L1 Class Work

---
+++ Unit 1 - Introduction to C#/Lesson8Project/Lesson8Project/Program.cs
@@ -0,0 +1,68 @@
+﻿////////////////////////////////////////////////////////////////////////
+//
+// Unit 1, Lesson 8: Programming Project
+//
+// Random Number Guessing Game
+//
+// written by Jason Zhang
+// Data Structures, Period 5
+//
+////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace Lesson8ProgrammingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an application that generates a random number
+            // from 1 to 1000 and asks the user what the number is.
+            // If the user's guess is too high or too low, the 
+            // program should display an appropriate message, such 
+            // as "Too high" or "Too low".  However, if the guess
+            // is within 10, then the program should simply state
+            // that the user is "getting warmer".  If the user 
+            // guesses the number, congratulate the user and ask 
+            // if the user would like to play again.  If the user
+            // cannot guess the number with 10 or 12 tries, the 
+            // game ends without the congratulatory message but 
+            // the user is still prompted to play again.
+            //
+            // The game should keep certain statistics about the 
+            // session, including:
+            //
+            //    session win-loss record
+            //    session fastest time to solve
+            //    session minimum number of tries to solve
+            //
+            // This information should be displayed after each 
+            // game ends.  
+
+            Random r = new Random();
+            int rand = r.Next(1, 1000);
+            Console.WriteLine("Please Guess a Number from (1-1000)");
+
+            int guess = 0;
+
+            while (guess != rand)
+            {
+                while (!int.TryParse(Console.ReadLine(), out guess))
+                {
+                    Console.WriteLine("Invalid Input! Try Again!");
+                }
+                if (guess > rand)
+                {
+                    Console.WriteLine("Too high!");
+                }
+                if (guess < rand)
+                {
+                    Console.WriteLine("Too low!");
+                }
+            }
+            Console.WriteLine("Good Job! You Got It!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Unit 2 - Data and Text Processing/Lesson1ClassWork/Lesson1ClassWork/Program.cs
@@ -0,0 +1,81 @@
+﻿////////////////////////////////////////////////////////////////////////
+//
+// Unit 2, Lesson 1: Class Work
+//
+// Maximumal Sequence
+//
+// written by Jason Zhang
+// Data Structures, Period 5
+//
+////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace Lesson1ClassWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Array Length (1 to infinity)");
+
+            int arrayLength;
+
+            while (!int.TryParse(Console.ReadLine(), out arrayLength) || arrayLength < 1)
+            {
+                Console.WriteLine("Invalid Input! Try Again!");
+            }
+
+            int[] sequence = new int[arrayLength];
+
+            int maxlength = 0;
+            int length = 1;
+            int index = 0;
+            int previous = 0;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                int element;
+                Console.WriteLine("Enter element at index {0} below", i);
+                while (!int.TryParse(Console.ReadLine(), out element))
+                {
+                    Console.WriteLine("Invalid Input! Try Again!");
+                }
+                if (i > 0)
+                {
+                    if (element > previous)
+                    {
+                        length++;
+                    }
+                    else
+                    {
+                        if (length > maxlength)
+                        {
+                            maxlength = length;
+                            index = i - length - 1;
+                        }
+                        length = 1;
+                    }
+                }
+
+                if (length > maxlength)
+                {
+                    maxlength = length;
+                    index = i - length + 1;
+                }
+
+                previous = element;
+                sequence[i] = element;
+            }
+
+            int[] subSequence = new int[maxlength];
+            for (int i = 0; i < maxlength;  i++)
+            {
+                subSequence[i] = sequence[i + index];
+            }
+
+            Console.WriteLine("Maximum increasing integers is [" + string.Join(", ", subSequence) + "]");
+            Console.ReadLine();
+        }
+    }
+}
