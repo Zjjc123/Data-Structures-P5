7869be6d3079e5f29b6c3fb123e48839ce997d1e
Jason Zhang <zjjc123@hotmail.com>
2020-11-12 13:56:42 -0800

    Lesson 2 Class Work

---
+++ Unit 2 - Data and Text Processing/Lesson2ExitTicket/Lesson2ExitTicket/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Lesson2ExitTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter square array size");
+            int s;
+            while (!int.TryParse(Console.ReadLine(), out s) || s <= 1)
+            {
+                Console.WriteLine("Invalid Number!");
+            }
+
+            int[,] square = new int[s, s];
+            for (int i = 0; i < square.GetLength(0); i++)
+            {
+                Console.WriteLine("Enter {0} Row", i + 1);
+                for (int j = 0; j < square.GetLength(1); j++)
+                {
+                    int.TryParse(Console.ReadLine(), out square[i, j]);
+                }
+            }
+
+            for (int i = 0; i < square.GetLength(0); i++)
+            {
+                for (int j = 0; j < square.GetLength(1); j++)
+                {
+                    Console.Write($"{square[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+            int[] checkSums = new int[2 * s + 2];
+
+            int diagonal = 0;
+            int diagonal2 = 0;
+
+            for (int i = 0; i < square.GetLength(0); i++)
+            {
+                diagonal += square[i, i];
+            }
+            for (int i = 0; i < square.GetLength(0); i++)
+            {
+                diagonal2 += square[i, s - i - 1];
+            }
+            
+            for (int i = 0; i < square.GetLength(0); i++)
+            {
+                int currentRow = 0;
+                for (int j = 0; j < square.GetLength(1); j++)
+                {
+                    currentRow += square[i, j];
+                }
+                checkSums[i] = currentRow;
+
+                Console.WriteLine("Row: {0}", currentRow);
+            }
+
+            for (int i = 0; i < square.GetLength(0); i++)
+            {
+                int currentColumn = 0;
+                for (int j = 0; j < square.GetLength(1); j++)
+                {
+                    currentColumn += square[j, i];
+                }
+                checkSums[s + i] = currentColumn;
+
+                Console.WriteLine("Column: {0}", currentColumn);
+            }
+
+            Console.WriteLine("Diagonal: {0}", diagonal);
+            Console.WriteLine("Diagonal: {0}", diagonal2);
+          
+            checkSums[checkSums.Length - 1] = diagonal;
+            checkSums[checkSums.Length - 2] = diagonal2;
+
+            Console.WriteLine(string.Join(", ", checkSums));
+
+            bool isMagic = true;
+
+            for (int i = 1; i < checkSums.Length; i++)
+            {
+                Console.WriteLine(checkSums[i] + " --> " + checkSums[i-1]);
+                if (checkSums[i] != checkSums[i - 1])
+                {
+                    Console.WriteLine(checkSums[i]);
+                    isMagic = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine("Magic Square: " + isMagic);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Unit 2 - Data and Text Processing/Lesson2Sandbox/Lesson2Sandbox/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson2Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] x = new int[4, 6];
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for (int j = 0; j < x.GetLength(1); j++)
+                {
+                    Console.Write($"{x[i, j]: F} ");
+                }
+                Console.WriteLine();
+            }
+
+            int[][] jagged2D = new int[4][];
+            jagged2D[0] = new int[6];
+            jagged2D[1] = new int[6];
+            jagged2D[2] = new int[6];
+            jagged2D[3] = new int[6];
+
+            Console.ReadLine();
+        }
+    }
+}
