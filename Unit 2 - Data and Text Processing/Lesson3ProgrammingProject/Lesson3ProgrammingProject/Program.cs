48cefc060612c5980beac8aae5eb64ee7b6d9ed7
Jason Zhang <zjjc123@hotmail.com>
2020-11-19 12:23:15 -0800

    Basic Lesson 3 Programming Project

---
+++ Unit 2 - Data and Text Processing/Lesson3ProgrammingProject/Lesson3ProgrammingProject/Program.cs
@@ -0,0 +1,34 @@
+﻿////////////////////////////////////////////////////////////////////////
+//
+// Unit 2, Lesson 3: Programming Project
+//
+// Financial Planning Options
+//
+// written by {$NAME}
+// Data Structures, Period {$PERIOD}
+//
+////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace Lesson3ProgrammingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that presents a menu of numbered options 
+            // for financial planning.  The program will ask the user to
+            // select one of the options numbered below.When selected,
+            // each option will prompt the user for the information
+            // required for the task, process the information and do the
+            // calculations, then display the results to the user.  The
+            // program will then wait for the user to review the 
+            // information.  Upon review, the program will clear the 
+            // console and then return to the main menu. The user can 
+            // then select another option or choose an option that exits
+            // the program.
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
