5255a8dd92a738ae09a8c851c89c2a9bb7c3ac89
Jason Zhang <zjjc123@hotmail.com>
2020-11-19 14:36:14 -0800

    Part 1

--- Unit 2 - Data and Text Processing/Lesson3ProgrammingProject/Lesson3ProgrammingProject/Program.cs
+++ Unit 2 - Data and Text Processing/Lesson3ProgrammingProject/Lesson3ProgrammingProject/Program.cs
@@ -17,18 +17,84 @@ namespace Lesson3ProgrammingProject
     {
         static void Main(string[] args)
         {
-            // Write a program that presents a menu of numbered options 
-            // for financial planning.  The program will ask the user to
-            // select one of the options numbered below.When selected,
-            // each option will prompt the user for the information
-            // required for the task, process the information and do the
-            // calculations, then display the results to the user.  The
-            // program will then wait for the user to review the 
-            // information.  Upon review, the program will clear the 
-            // console and then return to the main menu. The user can 
-            // then select another option or choose an option that exits
-            // the program.
+            bool exit = false;
+            
+            while (!exit)
+            {
+                int pick = 0;
+
+                Console.WriteLine(
+                    "   1. Mortgage Monthly Payment Calculator\r\n" +
+                    "   2. Remaining Mortgage Loan Balance Calculator\r\n" +
+                    "   3. Remaining Mortgage Loan Balance Yearly Report\r\n" +
+                    "   4. Future Value of a Fixed Asset\r\n" +
+                    "   5. Retirement Account Value Estimate\r\n" +
+                    "   6. Retirement Account Value Yearly Report\r\n" +
+                    "   7. Exit\r\n");
+
+                Console.WriteLine("Enter Option Below...");
+
+                while (!int.TryParse(Console.ReadLine(), out pick) || pick < 1 || pick > 7)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+
+                switch (pick)
+                {
+                    case 1:
+                        int amountBorrowed = GetUserInteger("Amount borrowed (in dollars)");
+                        int termsOfMortgage = GetUserInteger("Term of mortgage (in years)");
+                        int yearlyInterestRate = GetUserInteger("Yearly interest rate (as a percentage)");
+
+                        MortgageMonthlyPaymentCalculator(amountBorrowed, termsOfMortgage, yearlyInterestRate);
+
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        exit = true;
+                        break;
+                }
+            }
+
             Console.WriteLine("Hello World!");
         }
+
+        static int GetUserInteger(string prompt)
+        {
+            int i = 0;
+
+            Console.WriteLine(prompt);
+
+            while (!int.TryParse(Console.ReadLine(), out i))
+            {
+                Console.WriteLine("Invalid Number!");
+            }
+
+            return i;
+        }
+
+        static void MortgageMonthlyPaymentCalculator(int p, int n, int r)
+        {
+            double rate = r / 100.0 / 12;
+            double months = n * 12;
+            double monthlyMortgageAmount = p * (rate * Math.Pow(1 + rate, months) / (Math.Pow(1 + rate, months) - 1));
+            double totalAmountPaid = monthlyMortgageAmount * n * 12;
+            double totalInterestPaid = totalAmountPaid - p;
+
+            Console.WriteLine("Monthly Mortgage Amount: {0,20:c}", monthlyMortgageAmount);
+            Console.WriteLine("Total Amount Paid:       {0,20:c}", totalAmountPaid);
+            Console.WriteLine("Total Interest Paid:     {0,20:c}", totalInterestPaid);
+            Console.WriteLine();
+        }
+        
     }
 }
